aabeacb4a53d1f60de2ff36bc098dabf91f10164
Futureland3 <markmurai3@gmail.com>
2022-12-27 11:32:09 +0100

    Mostly fixed header tilt bug

--- javascript.js
+++ javascript.js
@@ -3,10 +3,30 @@ $(document).ready(function(){
         $(this).toggleClass("active");
         $(".links").toggleClass("active");
         $(".links a").toggleClass("active");
+        $(".links").css("transition", "1s ease");
+        setTimeout(function(){
+            $(".links").css("transition", "");
+        },3000);
+        // $(".links")
+        //     .css("transition", "1s ease")
+        //     .delay(3000)
+        //     .queue(function(){
+        //         $(".links").css("transition", "");
+        //     });
     });
 });
 
 // $('#user_button').toggle().css('borderBottomLeftRadius','+5px');
 
+// setTimeout(function(){
+//     $(formMessages).removeClass('error');
+//     //....and whatever else you need to do
+// }, 3000);
 
-
+// $(formMessages)
+//   .addClass('error')
+//   .delay(3000)
+//   .queue(function(next){
+//     $(this).removeClass('error');
+//     next();
+//   })
